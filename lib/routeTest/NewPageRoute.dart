4bc2b9bf95b80507d902b64bd1c51b7c79d65f40
chong.yang <chong.yang@sodacar.com>
2020-03-04 18:06:10 +0800

    路由传值，取值， 加载本地图片

--- lib/routeTest/NewPageRoute.dart
+++ lib/routeTest/NewPageRoute.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/routeTest/MainPage.dart';
 
 class NewRoute extends StatelessWidget {
+  NewRoute({Key key, this.product}) : super(key: key);
+  final Product product;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("New route"),
-      ),
-      body: Center(
-        child: Text("This is new route"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("${product.title}"),
+        ),
+        body: Column(
+          children: <Widget>[
+            //加载本地图片，
+            //1.先在pubspec.yaml文件注册文件， 在用asset方法显示图片
+            Image.asset(
+              'images/avater.jpg',
+              width: 300,
+              height: 300,
+              fit: BoxFit.fitHeight,
+            ),
+            RaisedButton(
+              child: Text('返回上一页'),
+              onPressed: () {
+                Navigator.pop(context, "返回的内容");
+              },
+            ),
+          ],
+        ));
   }
 }
